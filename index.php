bd9c69ccaf8ff97422db17819b419f7d30d28bd2
Istifanus Alexander1 <istifanusalexander@gmail.com>
2025-11-12 15:00:06 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+    <style>
+        *{
+            box-sizing: border-box;
+            font-family: sans-serif;
+            margin: 0;
+            padding: 0;
+        }
+        body{
+            background-color: #e1e1e184;
+            justify-content: center;
+            height: 100vh;
+            align-items: center;
+            display: flex;
+            flex-direction: column;
+        }
+        .proceed{
+            color: #fff;
+            background-color: #0300b4;
+            padding: 15px;
+            border-radius: 7px;
+            font-size: 17px;
+            font-weight: 700;
+            text-decoration: none;
+            transition: all 0.2s;
+            box-shadow: 0 0 10px #5754efff;
+        }
+        .proceed:hover{
+            text-decoration: underline;
+            background-color: #5351c2ff;
+        }
+    </style>
+</head>
+<body>
+    <nav>
+        
+    </nav>
+    <div>
+        <h1>Welcome To The Nigeran Army New Recruitment Portal</h1><br>
+        <a class="proceed" href="apply/index.php">Proceed to portal</a>
+    </div>
+</body>
+</html>
+<!-- https://www.jotform.com/form-templates/new-customer-registration-form
+https://www.123formbuilder.com/free-form-templates/pet-adoption-event-application-form -->
+<!-- https://www.symbolspy.com/peace-sign-symbol.html -->
+
+
