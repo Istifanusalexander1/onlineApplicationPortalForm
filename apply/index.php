2e4e0eee0ee14a1686c263b80855f8d3d6cc1c3e
Istifanus Alexander1 <istifanusalexander@gmail.com>
2025-11-14 10:14:53 +0000

    this is the new update

--- apply/index.php
+++ apply/index.php
@@ -16,19 +16,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Online Application Portal Form</title>
-    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="indexs.css">
 </head>
 <body>
     <nav>
         <h1>Hi🙋 <?php echo $_SESSION['email']; ?> Welcome.</h1>
-        <p><?php echo $_SESSION['email'][0]; ?></p>
+        <p class="log"><?php echo $_SESSION['email'][0]; ?></p>
+        <div class="logout-div">
+            <form action="<?php $_SERVER['PHP_SELF'] ?>" method="post">
+                <input class="logout" type="submit" value="Log-Out" name="logout">
+            </form>
+        </div>
     </nav>
 
-    <div class="logout-div">
-        <form action="<?php $_SERVER['PHP_SELF'] ?>" method="post">
-            <input class="logout" type="submit" value="Log-Out" name="logout">
-        </form>
-    </div>
 
 </body>
 </html>
